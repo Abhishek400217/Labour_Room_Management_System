3c8d30da09b744cdc784ecfcdc63303a526e3882
Abhishek Karande <abhishekkarande17@gmail.com>
2026-07-29 13:32:15 +0530

    added demo folder for testing

---
+++ LRMS_API/Demo/Class.cs
@@ -0,0 +1,13 @@
+﻿namespace LRMS_API.Demo
+{
+    public class Class
+    {
+        FormatException FormatException { get; set; }
+        public Class() { }  
+        public Class(string message) { }
+        public Class(string message, Exception exception)
+        {
+
+        }
+    }
+}
