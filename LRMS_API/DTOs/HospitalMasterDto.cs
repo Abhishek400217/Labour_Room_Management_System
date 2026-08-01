0cb2daf758c5cdd75160726b238a009afc5a6786
Abhishek Karande <abhishekkarande17@gmail.com>
2026-08-01 16:29:42 +0530

    Added Login API, Hospital Master and Doctor Master module

---
+++ LRMS_API/DTOs/HospitalMasterDto.cs
@@ -0,0 +1,22 @@
+﻿public class HospitalMasterDto
+{
+    public int HospitalId { get; set; }
+
+    public string HospitalCode { get; set; } = string.Empty;
+
+    public string HospitalName { get; set; } = string.Empty;
+
+    public string? Address { get; set; }
+
+    public string? City { get; set; }
+
+    public string? State { get; set; }
+
+    public string? Pincode { get; set; }
+
+    public string? ContactNo { get; set; }
+
+    public string? Email { get; set; }
+
+    public bool IsActive { get; set; }
+}
