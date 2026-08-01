83c510ded739bed8c4e6e2b77c0a285d00f62411
Abhishek Karande <abhishekkarande17@gmail.com>
2026-08-01 15:20:21 +0530

    PAtient module added

---
+++ LRMS_API/DTOs/PatientDTO.cs
@@ -0,0 +1,28 @@
+﻿namespace LRMS_API.DTOs;
+
+public class PatientDTO
+{
+    public string UHID { get; set; } = "";
+
+    public string FirstName { get; set; } = "";
+
+    public string MiddleName { get; set; } = "";
+
+    public string LastName { get; set; } = "";
+
+    public string HusbandName { get; set; } = "";
+
+    public int Age { get; set; }
+
+    public string Gender { get; set; } = "";
+
+    public string MobileNo { get; set; } = "";
+
+    public string BloodGroup { get; set; } = "";
+
+    public string Village { get; set; } = "";
+
+    public string Taluka { get; set; } = "";
+
+    public string District { get; set; } = "";
+}
---
+++ LRMS_API/Repositories/IPatientRepository.cs
@@ -0,0 +1,8 @@
+﻿using LRMS_API.DTOs;
+
+namespace LRMS_API.Repositories;
+
+public interface IPatientRepository
+{
+    Task<int> AddPatientAsync(PatientDTO dto);
+}
