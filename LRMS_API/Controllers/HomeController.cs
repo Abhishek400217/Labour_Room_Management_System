fd54964e1c0d65b8d335f5a625c2123f0d4f7c93
Abhishek Karande <abhishekkarande17@gmail.com>
2026-08-04 16:48:51 +0530

    Setup MVC structure and login page architecture

---
+++ LRMS_API/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LRMS_API.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Login/Index.cshtml");
+        }
+    }
+}
